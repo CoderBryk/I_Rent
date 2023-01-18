cf86c3eaa736e9ac86aef0ae285a1134636d101f
BlossomEze <blossomeze0@gmail.com>
2023-01-18 11:11:55 +0100

    next commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from './Components/Footer'
 import Contact from './Components/Contact'
 import Home from './Components/Home'
 import Terms from './Components/Terms'
-import Signup1 from './Components/Signup1'
+import Signup from './Components/Signup'
 import Login from './Components/Login'
 import PrivacyPolicy from './Components/PrivacyPolicy'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -17,10 +17,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login/" element={<Login />} />
+        <Route path="signup/" element={<Signup />} />
         <Route path="contact_us/" element={<Contact />} />
         <Route path="terms_and_conditions/" element={<Terms />} />
         <Route path="privacy_policy/" element={<PrivacyPolicy />} />
-        <Route path="signup/" element={<Signup1 />} />
       </Routes>
       <Footer />
     </BrowserRouter>
