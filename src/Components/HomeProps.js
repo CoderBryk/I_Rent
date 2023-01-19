e49be2dc8561e92703755dd3a404abac59bd0194
BlossomEze <blossomeze0@gmail.com>
2023-01-19 21:32:54 +0100

    main

--- src/Components/HomeProps.js
+++ src/Components/HomeProps.js
@@ -6,53 +6,40 @@ function HomeProps() {
   return (
     <div>
         <div className='bg-[#dff4ff59] justify-between'>
-            <div className='flex justify-between items-center py-[3%] px-[5%]'>
-                <h1 className='text-3xl font-bold'>Top Liked Houses</h1>
-                <h3 className='text-xl font-semibold'>Load More</h3>
+            <div className='flex justify-between items-center pt-[5%] pb-[5%] px-[5%]'>
+                <h1 className='text-base lg:text-3xl font-bold'>Top Liked Houses</h1>
+                <h3 className='text-sm lg:text-xl font-semibold'>Load More</h3>
             </div>
-            <div className='flex flex-wrap'>
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-            </div>
-        </div>
-        <div className='bg-gradient-[radial-gradient(51.56% 51.56% at 50% 48.44%, #265DB7 0%, #264D99 79.34%, #07327D 98.61%)] justify-between'>
-            <div className='flex justify-between items-center py-[3%] px-[5%]'>
-                <h1 className='text-3xl font-bold'>Top Liked Houses</h1>
-                <h3 className='text-xl font-semibold'>Load More</h3>
-            </div>
-            <div className='flex flex-wrap'>
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
+            <div className='flex flex-wrap justify-evenly'>
+                <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
+                <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
+                <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
+                <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
             </div>
         </div>
-        <div className='bg-[#dff4ff59] justify-between'>
-            <div className='flex justify-between items-center py-[3%] px-[5%]'>
-                <h1 className='text-3xl font-bold'>Top Liked Houses</h1>
-                <h3 className='text-xl font-semibold'>Load More</h3>
-            </div>
-            <div className='flex flex-wrap'>
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-            </div>
+        <div className='blue'>
+        <div className='flex justify-between items-center pt-[5%] pb-[5%] px-[5%]'>
+        <h1 className='text-base lg:text-3xl font-bold'>Hot self contains 🔥</h1>
+        <h3 className='text-sm lg:text-xl font-semibold'>Load More</h3>
         </div>
-        <div className='bg-[#dff4ff59] justify-between'>
-            <div className='flex justify-between items-center py-[3%] px-[5%]'>
-                <h1 className='text-3xl font-bold'>Top Liked Houses</h1>
-                <h3 className='text-xl font-semibold'>Load More</h3>
+        <div className='flex flex-wrap justify-evenly'>
+        <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
+        <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
+        <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
+        <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
+    </div>
+    </div>
+    <div className='bg-[#dff4ff59] justify-between'>
+            <div className='flex justify-between items-center pt-[5%] pb-[5%] px-[5%]'>
+                <h1 className='text-base lg:text-3xl font-bold'>Single rooms</h1>
+                <h3 className='text-sm lg:text-xl font-semibold'>Load More</h3>
             </div>
-            <div className='flex flex-wrap'>
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
-                <Props image="../Assets/house.png" type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' />
+            <div className='flex flex-wrap justify-evenly'>
+                <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
+                <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
+                <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
+                <Props image={ house } type='Duplex' price='500' title='Fully Furnished Duplex for Sale' location='Town campus, Uyo' width='500m' dimensions='10x10m' description='4 bedroom'  />
             </div>
-            
         </div>
     </div>
   )
--- src/Components/Props.js
+++ src/Components/Props.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import { LocationOn } from '@mui/icons-material'
+import { Equalizer, LegendToggle, LocationOn, Hotel } from '@mui/icons-material'
+import '../Stylesheets/Home.css'
 
-function Props({image, type, price, title, location}) {
+function Props({image, type, price, title, location, width, dimensions, description}) {
   return (
-    <div className='max-w-[48%]'>
-        <div>{ image }</div>
-        <div className='absolute justify-between'>
-            <h4>{ type }</h4>
-            <h4>${ price }</h4>
+    <div className='max-w-[47%] bg-white rounded-[10px] pb-2 mb-6'>
+      <div className='flex justify-around absolute pt-[115px] w-[47%]'>
+        <h4 className='bg-primary rounded-[10px] px-3 py-[2px]  text-white text-[12px]'>{ type }</h4>
+        <h3 className='bg-white text-primary rounded-[10px] px-3 py-[2px] text-[12px]'>${ price }</h3>
+      </div>
+        <img className='max-w-[45vw]' src={ image } alt="Building" onError={(e)=>{e.target.onerror=null; e.target.src="../Assets/house.png"}} />
+        <h3 className='text-[11px] font-bold pt-5 text-center'>{ title }</h3>
+        <h3 className='text-[10px] pb-2 font-bold text-center'><LocationOn />{ location }</h3>
+        <div className='flex text-[#9A9A9A] justify-between'>
+          <h4 className='text-[10px] flex'><Equalizer />{ width } </h4>
+          <h4 className='text-[10px] flex'><LegendToggle />{ dimensions }</h4>
+          <h4 className='text-[10px] flex'><Hotel />{ description }</h4>
         </div>
-        <h3>{ title }</h3>
-        <h3><LocationOn />{ location }</h3>
-        <div>
-            <button>Add to wishlist</button>
-            <button>Book now</button>
+        <div className='flex justify-between py-3 px-2'>
+            <button className='border-primary border-2 text-[9px] rounded-[10px] w-[48%] py-1 px-[2px]'>Add to wishlist</button>
+            <button className='bg-primary text-[9px] text-white rounded-[10px] w-[48%] py-1 px-[2px]' >Book now</button>
         </div>
     </div>
   )
