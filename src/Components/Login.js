1a33b6ec0574c4d4a71a6c68f12696e2cbf164fb
BlossomEze <blossomeze0@gmail.com>
2023-02-09 23:45:40 +0100

    main

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,27 +1,48 @@
-import React from 'react'
-import shortf from '../assets/shortf.png'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import forms from '../Assets/forms.png'
+import { Link } from 'react-router-dom';
+import background from  '../Assets/bd3.png'
 
-function Login() {
+function Signup() {
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div className='flex'>
-        <div className='flex-[0.4]'> 
-            <img className='max-h-[80vh] min-w-[40vw]' src={ shortf } alt=' a building' />
+      <div className='hidden lg:flex flex-[0]'> 
+      <img className='max-h-[120vh] min-w-[40vw]' src={ forms } alt=' a building' />
+      </div>
+      <div  style={{
+        backgroundImage: `url(${isDesktop ? 'aliceblue' : background })`
+      }}  className='px-[7%] lg:px-[15%] pt-[80px] lg:pt-0 pb-[8%] bg-[aliceblue] justify-center text-center flex-[1]'>
+      <div className=' bg-[aliceblue] lg:bg-none px-6 pb-9 rounded-[18px] lg;rounded-0'>
+      <h1 className='font-bold text-primary text-xl lg:text-3xl leading-[45px] pt-5 pb-6 justify-center'>Welcome Back</h1>
+        <div className='text-left flex flex-col'>
+                <h1 className='pt-2 pb-2'>Email</h1>
+                <input type='email' placeholder='Enter your email' className='w-full py-[3vh]  pl-[1.5vw] lg:pl-[3vh]  h-[5vh] mb-4 rounded-[10px] shadow [1px 4px 54px 13px rgba(0, 0, 0, 0.08)] bg-white' />
+                <h1 className='pt-2 pb-2'>Password</h1>
+                <input type='text' placeholder='Enter your password' className='w-full py-[3vh]  pl-[1.5vw] lg:pl-[3vh]  h-[5vh] mb-4 rounded-[10px] shadow [1px 4px 54px 13px rgba(0, 0, 0, 0.08)] bg-white'  />
+                </div>      
+                <h3 className='text-sm text-orange py-3'>Forgot password?</h3>
+                <div className= 'flex pt-[5%] justify-around mb-5 '>
+                  <Link to='/finish_signup'><button className='bg-primary rounded-[10px] text-white px-5 py-2 text-[12px]'>
+                    Create Account
+                  </button></Link>
+                  <button className='bg-white rounded-[10px] text-primary px-8  text-[12px] py-2 shadow [1px 4px 54px 13px rgba(0, 0, 0, 0.08)]'><Link to='/login'>Login Now</Link></button>
+                </div>
         </div>
-        <div className='px-[15%] py-[8%] bg-[aliceblue] flex-[0.6]'>
-        <h1 className='font-bold text-primary text-3xl leading-[45px] pb-6 justify-center'>Welcome Back</h1>
-            <h1 className='pt-2 pb-2'>Email</h1>
-            <input type='email' placeholder='Enter your email' className='w-full py-[3vh] pl-[3vh]  h-[5vh] mb-4 rounded-[10px] shadow [1px 4px 54px 13px rgba(0, 0, 0, 0.08)] bg-white' />
-            <h1 className='pt-2 pb-2'>Password</h1>
-            <input type='text' placeholder='Enter your password' className='w-full py-[3vh] pl-[3vh]  h-[5vh] mb-4 rounded-[10px] shadow [1px 4px 54px 13px rgba(0, 0, 0, 0.08)] bg-white'  />
-            <div className= 'flex pt-[7%]'>
-              <button className='bg-primary rounded-[10px] text-white px-6 py-2 mr-[8%]'>Login in now</button>
-              <button className='bg-white rounded-[10px] text-primary px-6 py-2 shadow [1px 4px 54px 13px rgba(0, 0, 0, 0.08)]'><Link to='/signup'>Create acount</Link></button>
         </div>
-        </div>
-
     </div>
   )
 }
 
-export default Login
+export default Signup
